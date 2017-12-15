05681162c17f2c32a67880644a83927e57742aeb
Desiree Rainey <Drainey21.DR@gmail.com>
2017-12-15 11:43:05 -0800

    Win/lose function created. Need to commit more

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // create an array of Words
-const word = ['loki', 'hulk', 'thor', 'ant-man', 'spider-man' ]
+const word = ['hello'] //, 'loki', 'hulk', 'thor', 'cable', 'spider-man', 'magneto', 'mystique', 'rogue' ]
 //choose word randomly
 var randNum = Math.floor(Math.random() * word.length);
 var rightWord = [];
@@ -8,7 +8,9 @@ var choosenWord = word[randNum];
 var underScore = [];
 var docUnderScore = document.getElementsByClassName('underscore');
 var docRightGuess= document.getElementsByClassName('rightGuess');
-var docWrongGuess = document.getElementsByClassName('wrongGuess')
+var docWrongGuess = document.getElementsByClassName('wrongGuess');
+var wrongCount = 0;
+var MAX_WRONG_ALLOWED = 5;
 
 console.log(choosenWord);
 //create underscores based on the length of the array
@@ -24,32 +26,40 @@ console.log(choosenWord);
 //if users guess is right
 document.addEventListener('keypress', function(){
 	var keyword = String.fromCharCode(event.keyCode);
-	
+//takes generated words index from event starting fom 0 
+//pushes rightWord to array on screen
 	if(choosenWord.indexOf(keyword) > -1){
 		rightWord.push(keyword);
-//replaces underscore 
+// replaces underscore with right letter
 		underScore[choosenWord.indexOf(keyword)] = keyword;
 		docUnderScore[0].innerHTML = underScore.join(' ');
+//pushes letter to right word array		
 		docRightGuess[0].innerHTML = rightWord;
 
 		if(underScore.join('') == choosenWord){
-			alert('You Win');
+			swal("You Win!");
+			// alert('You Win');
 		}
 	}else {
 		wrongWord.push(keyword);
+		wrongCount++
+		if(wrongCount > MAX_WRONG_ALLOWED){
+			swal("You lose!");
+			//do lose condition/game over stuff here
+		}
+//pushes letter to wrong word array		
 		docWrongGuess[0].innerHTML = wrongWord
 		console.log(wrongWord);
 	}
 });
 
+
 docUnderScore[0].innerHTML = generateUnderscore().join(' ');
 
-//check each letter guess to see if it is right
 
-//if right push to right box (store in array??)
-
-//if wrong push to wrong box (? store in an array)
-
+function reloadGame() {
+  location.reload()
+}
 // for two players create score system ??
 // or change color of text for player 
 //when text is pushed to a box
