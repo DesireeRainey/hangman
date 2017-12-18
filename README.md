f94d6a4ff208c0a361ed42e077e7c4ff2d24c918
Desiree Rainey <Drainey21.DR@gmail.com>
2017-12-18 08:53:52 -0800

    final game with read me

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # hangman
-A game of Hangman 
+A game of guess the Superhero. Press any key to guess letters of the displayed underscore superhero game. Six wrong guesses and you lose. Guess every letter and you become the winner! 
+
+This game was built with HTML, CSS, Javascript and Sweet Alert pop up messages. 
+
+Hosted on GitHub Pages.  
+
+Approach Taken:
+
+Started by building the basic game structure. 
+	Places for right and wrong answers to live along with the choosen word div. 
+
+Next I worked on setting up a funtion that choose a random word from a preselected array of superheroes. 
+
+From here the generateUnderscore function was created to take the length of the choosen word and generate underscores based on that length in the console log. 
+
+The next function handles the users guess by checking the key pressed against the underscores in the choosen word array. If it is correct the letter is pushed to the underscore and the right letter array. If it is incorrect the letter is counted and pushed to the wrong letter array. 
+
+If there are 6 wrong guesses before the word is completed the user will lose the game. If all of the letters in the choosen word are guessed correctly the user can play again after dismissing the sweet alert pop up. 
+
+Issues: 
+
+Max wrong number of allowed guess triggering lose alert.  
+Event Listener keypress functions. 
+
+Resources Used: 
+
+Font: https://fonts.google.com/?utm_source=google&utm_medium=cpc&utm_campaign=1001467+|+Material.IO+|+Global+|+en+|+Hybrid+|+Text+|+BKWS&utm_term=%7Bkeyword%7D&gclid=CjwKCAiA693RBRAwEiwALCc3u3U0FW4_05v6Ko_deT6a31WaPD9tKrtBtkKoUwUoYmwrlN9CJtrDBBoCVFMQAvD_BwE&selection.family=Bungee
+Sweet Alert: https://sweetalert.js.org/guides/
+Backgroud: https://wallpaper.wiki/marvel-backgrounds-free-download.html/marvel-wallpaper-pictures-images
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // create an array of Words
-const word = ['hello'] //, 'loki', 'hulk', 'thor', 'cable', 'spider-man', 'magneto', 'mystique', 'rogue' ]
+const word = ['loki', 'hulk', 'thor', 'cable', 'spider-man', 'magneto', 'mystique', 'rogue' ]
 //choose word randomly
 var randNum = Math.floor(Math.random() * word.length);
 var rightWord = [];
@@ -31,37 +31,27 @@ console.log(choosenWord);
 	var guess = function(){
 	var keyword = String.fromCharCode(event.keyCode);
 //takes generated words index from event starting fom 0 
-	
-	for(var i = 0; i < choosenWord.length; i++){
-		// if(i > choosenWord.length) break;
-		if(choosenWord.indexOf(keyword) > i){
-
-		rightWord.push(keyword);
-// replaces underscore with right letter
-		underScore[choosenWord.indexOf(keyword)] = keyword;
-
-
-		docUnderScore[0].innerHTML = underScore.join(' ');
-//pushes letter to right word array		
-		docRightGuess[0].innerHTML = rightWord;
-	}
-//need for loop that runs through then whole index and doesnt stop
+		if(choosenWord.indexOf(keyword) > -1){
+			rightWord.push(keyword);
+			// replaces underscore with right letter
+			underScore[choosenWord.indexOf(keyword)] = keyword;
+			docUnderScore[0].innerHTML = underScore.join(' ');
+			//pushes letter to right word array		
+			docRightGuess[0].innerHTML = rightWord;
 		if(underScore.join('') == choosenWord){
 			swal("You Win!");
-			
+		}	
 	}else {
 		wrongWord.push(keyword);
 		wrongCount++
 		if(wrongCount > MAX_WRONG_ALLOWED){
 			swal("You lose!");
-			//do lose condition/game over stuff here
 		}
 //pushes letter to wrong word array		
 		docWrongGuess[0].innerHTML = wrongWord
 		console.log(wrongWord);
 		}
-	}
-};
+	};
 
 document.addEventListener('keypress', guess);
 docUnderScore[0].innerHTML = generateUnderscore().join(' ');
@@ -70,6 +60,3 @@ docUnderScore[0].innerHTML = generateUnderscore().join(' ');
 function reloadGame() {
   location.reload()
 }
-// for two players create score system ??
-// or change color of text for player 
-//when text is pushed to a box
